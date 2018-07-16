5bb6f335044d5a7c36eed07cdbfdce9e80bbbd28
kh0rn <feracorp888@mail.ru>
2018-07-16 12:24:38 +0500

    Revert "init progect"
    This reverts commit b6ceba6b9e419c82597a57f41bd34307a9d573f4.

--- Source/DlgExample/DlgExample.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-#include "DlgExample.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DlgExample, "DlgExample" );
-
-DEFINE_LOG_CATEGORY(LogDlgExample)
- 
